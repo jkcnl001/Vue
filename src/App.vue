cfa0b3a697ff9e3a4ef758b2b2d4c7501cf36e3a
chuhouding <jkcnl001@163.com>
2020-09-17 20:53:29 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="counter">Counter: {{ counter }}</div>
+  </div>
+  <div id="bind-attribute">
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </div>
+  <div id="event-handling">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+  <div id="two-way-binding">
+    <p>{{ message }}</p>
+    <input v-model="message" />
+  </div>
+  <div id="conditional-rendering">
+    <span v-if="seen">Now you see me</span>
+  </div>
+  <div id="todo-list-app">
+    <ol>
+      <li v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id">{{ item.text}}</li>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      counter: 0,
+      message: "You loaded this page on " + new Date().toLocaleString(),
+      seen: true,
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++;
+      this.seen = !this.seen;
+    }, 1000);
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+};
 </script>
 
 <style>
